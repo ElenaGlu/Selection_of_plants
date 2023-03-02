0fa2bf626f5c4f0a17918087eaee8f9dce7830e2
lenochka <shishalovae@mail.ru>
2023-03-02 19:39:16 +0500

    working with a page 1

--- project/finder/management/commands/file.py
+++ project/finder/management/commands/file.py
@@ -7,8 +7,16 @@ def get_data(html):
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.quit()
-    print(1)
-    for link in soup.find_all('a', class_='title'):
+
+    list_of_page=[]
+    for page in range(1,62):
+        if page == 1:
+            list_of_page.append('https://leplants.ru/choose-plant/?utm_source=feature&utm_medium=choose&utm_campaign=share&lfilter=type%3A7&sort=-viewers')
+        else:
+            list_of_page.append(f'https://leplants.ru/choose-plant/?utm_source=feature&utm_medium=choose&utm_campaign=share&lfilter=type%3A7&page={page}sort=-viewers')
+
+        for link in soup.find_all('a', class_='title'):
+
         print('https://leplants.ru' + link.get('href'))
 def main():
     get_data('html')
