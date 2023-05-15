cf188eb8fe883ca64e8f65f8c880f8e205b4847c
ElenaGlu <shishalovae@mail.ru>
2023-05-15 22:22:50 +0500

    add static

--- project/finder/views.py
+++ project/finder/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import get_object_or_404, render
 from .models import HousePlants
+from django.core.paginator import Paginator
 
 
 def select_plant(request):
     plants = HousePlants.objects.all()
-    return render(request, 'finder/select_plant.html', {'plants': plants})
+    paginator = Paginator(plants, 30)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'finder/select_plant.html', {'page_obj': page_obj})
+
