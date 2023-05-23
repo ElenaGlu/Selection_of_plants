2e0abd36e2dc5b1db848e16c60b6561cc80b31f2
ElenaGlu <shishalovae@mail.ru>
2023-05-23 16:23:30 +0500

    update filter

--- project/finder/forms.py
+++ project/finder/forms.py
@@ -1,19 +1,73 @@
 from django import forms
 from .models import HousePlants
 
+
 class CheckBoxForm(forms.Form):
     unique_level_of_care = []
     list_level_of_care = []
+
+    unique_light_level = []
+    list_light_level = []
+
+    unique_irrigation_level = []
+    list_irrigation_level = []
+
+    unique_temperature = []
+    list_temperature = []
+
+    unique_humidity = []
+    list_humidity = []
+
+    unique_feeding = []
+    list_feeding = []
+
     all_plants = HousePlants.objects.all()
     for plant in all_plants:
         level_of_care = plant.level_of_care
         list_level_of_care.append(level_of_care)
-    unique_level_of_care = set(list_level_of_care)
-    CHOICES = tuple((item, item) for item in unique_level_of_care if item)
 
-    level_of_care = forms.MultipleChoiceField(label="Сложность ухода", widget=forms.CheckboxSelectMultiple, choices=CHOICES, required=False)
-    # light_level = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
-    # irrigation_level = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
-    # temperature = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
-    # humidity = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
-    # feeding = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
+        light_level = plant.light_level
+        list_light_level.append(light_level)
+
+        irrigation_level = plant.irrigation_level
+        list_irrigation_level.append(irrigation_level)
+
+        temperature = plant.temperature
+        list_temperature.append(temperature)
+
+        humidity = plant.humidity
+        list_humidity.append(humidity)
+
+        feeding = plant.feeding
+        list_feeding.append(feeding)
+
+    unique_level_of_care = set(list_level_of_care)
+    CHOICES1 = tuple((item, item) for item in unique_level_of_care if item)
+
+    unique_light_level = set(list_light_level)
+    CHOICES2 = tuple((item, item) for item in unique_light_level if item)
+
+    unique_irrigation_level = set(list_irrigation_level)
+    CHOICES3 = tuple((item, item) for item in unique_irrigation_level if item)
+
+    unique_temperature = set(list_temperature)
+    CHOICES4 = tuple((item, item) for item in unique_temperature if item)
+
+    unique_humidity = set(list_humidity)
+    CHOICES5 = tuple((item, item) for item in unique_humidity if item)
+
+    unique_feeding = set(list_feeding)
+    CHOICES6 = tuple((item, item) for item in unique_feeding if item)
+
+    level_of_care = forms.MultipleChoiceField(label="Сложность ухода", widget=forms.CheckboxSelectMultiple,
+                                              choices=CHOICES1, required=False)
+    light_level = forms.MultipleChoiceField(label="Освещенность", widget=forms.CheckboxSelectMultiple, choices=CHOICES2,
+                                    required=False)
+    irrigation_level = forms.MultipleChoiceField(label="Полив", widget=forms.CheckboxSelectMultiple, choices=CHOICES3,
+                                         required=False)
+    temperature = forms.MultipleChoiceField(label="Температура содержания", widget=forms.CheckboxSelectMultiple,
+                                    choices=CHOICES4, required=False)
+    humidity = forms.MultipleChoiceField(label="Влажность воздуха", widget=forms.CheckboxSelectMultiple, choices=CHOICES5,
+                                 required=False)
+    feeding = forms.MultipleChoiceField(label="Частота удобрения", widget=forms.CheckboxSelectMultiple, choices=CHOICES6,
+                                required=False)
--- project/finder/views.py
+++ project/finder/views.py
@@ -15,10 +15,32 @@ def select_plant(request):
         if form.is_valid():
             level_of_care = request.POST.getlist("level_of_care")
             plants = HousePlants.objects.filter(level_of_care__in=level_of_care)
+
+
+            light_level = request.POST.getlist("light_level")
+            plants2 = HousePlants.objects.filter(light_level__in=light_level)
+            plants.extend(plants2)
+
+            irrigation_level = request.POST.getlist("irrigation_level")
+            plants3 = HousePlants.objects.filter(irrigation_level__in=irrigation_level)
+            plants.extend(plants3)
+
+            temperature = request.POST.getlist("temperature")
+            plants4 = HousePlants.objects.filter(temperature__in=temperature)
+            plants.extend(plants4)
+
+            humidity = request.POST.getlist("humidity")
+            plants5 = HousePlants.objects.filter(humidity__in=humidity)
+            plants.extend(plants5)
+
+            feeding = request.POST.getlist("feeding")
+            plants6 = HousePlants.objects.filter(feeding__in=feeding)
+            plants.extend(plants6)
+
         else:
             CheckBoxForm()
-    paginator = Paginator(plants, 30)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+        paginator = Paginator(plants, 30)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
 
     return render(request, 'finder/select_plant.html', {'form': form, 'page_obj': page_obj})
