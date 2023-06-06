2ee4d4e99bdc55533b72c84ac3a38cb2df7fe82b
ElenaGlu <shishalovae@mail.ru>
2023-06-06 17:12:16 +0500

    added docstrings and type annotation to parsing.py

--- project/finder/management/commands/parsing.py
+++ project/finder/management/commands/parsing.py
@@ -1,15 +1,21 @@
-import os
-import psycopg2
-from finder import models
+from django.core.management.base import BaseCommand
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import json
 import urllib.request
-from django.core.management.base import BaseCommand
+import json
+
+from typing import List
+
+from finder import models
 
 
-def get_links_pagination():
+def get_links_pagination() -> List[str]:
+    """
+    Returns a list of links in the form of strings for pagination.
+    :return: Returns a list of links.
+    """
     list_links_pagination = [
         'https://leplants.ru/choose-plant/?utm_source=feature&utm_medium=choose&utm_campaign=share&lfilter=type%3A7'
         '&sort=-viewer']
@@ -20,7 +26,12 @@ def get_links_pagination():
     return list_links_pagination
 
 
-def get_links_to_all_items(list_links_pagination=None):
+def get_links_to_all_items(list_links_pagination: List[str])-> None:
+    """
+    Iterated through a list of links in the form of strings for pagination.
+    Collects links to all items and adds them to a new list.
+    Data from the new list is written to a json file.
+    """
     list_links_to_all_items = []
     driver = webdriver.Chrome()
     driver.set_window_size(1200, 600)
@@ -36,9 +47,16 @@ def get_links_to_all_items(list_links_pagination=None):
         file.write(js)
 
 
-def get_description_items():
-
-    list_dict_plant_info = []
+def get_description_items() -> List[dict]:
+    """
+    Iterated by links in the form of strings in a json file.
+    In each link, it collects data from a descriptive table and writes it to the dictionary.
+    Conversion of string values of two dictionary objects to the list data type.
+    Splitting a string into two values: min, max.
+    Creating a dictionary with data from a table for each element.
+    Creates a list from dictionaries.
+    :return: returns a list of dictionaries.
+    """
 
     s = Service('/home/elena/pythonProject/chromedriver.exe')
     driver = webdriver.Chrome(service=s)
@@ -47,10 +65,9 @@ def get_description_items():
         for link in json_urls:
             driver.get(link)
             soup = BeautifulSoup(driver.page_source, 'html.parser')
-
+            list_main_info_plant = []
             list_with_li = []
             initial_dict = {}
-
             table_about_plant = soup.find('div', class_='jn1ow0-0 bqYbNV')
             lines_with_tr = table_about_plant.find_all('tr')
             for tr in lines_with_tr:
@@ -85,7 +102,7 @@ def get_description_items():
                     initial_dict['min_height'] = 0
                     initial_dict['max_height'] = size[1]
 
-            dict_plant_info = {'name_of_plant': soup.find('h1', class_='hidden-xs').text,
+            main_info_plant = {'name_of_plant': soup.find('h1', class_='hidden-xs').text,
                                'pic_of_plant': soup.find('div', class_='item active').find('img').attrs['src'],
                                'min_height': initial_dict.get('min_height', None),
                                'max_height': initial_dict.get('max_height', None),
@@ -101,17 +118,25 @@ def get_description_items():
                                'temperature': initial_dict.get('Температура содержания', None),
                                'content_or_description': str(soup.find('div', class_='sc-4e9jew-13 cmbLRJ'))
                                }
-            list_dict_plant_info.append(dict_plant_info)
-        return list_dict_plant_info
+            list_main_info_plant.append(main_info_plant)
+        return list_main_info_plant
 
 
-def create_plant_instances(list_dict_plant_info):
+def create_plant_instances(list_main_info_plant: List[dict]) -> None:
+    """
+    Adding objects to the database.
+    :rtype: object
+    """
     list_plant_instances = []
-    for item in list_dict_plant_info:
+    for item in list_main_info_plant:
         list_plant_instances.append(models.HousePlants(**item))
     models.HousePlants.objects.bulk_create(list_plant_instances)
 
-def create_short_name():
+def create_short_name() -> None:
+    """
+    Creating a new field and adding a short name to it.
+    :rtype: object
+    """
     list_short_name = []
     for item in models.HousePlants.objects.all():
         name = item.name_of_plant
@@ -120,21 +145,26 @@ def create_short_name():
     models.HousePlants.objects.bulk_update(list_short_name, ['short_name'])
 
 
-def download_main_img():
-    all_plants = models.HousePlants.objects.all()
-    for plant in all_plants:
-        name_img = plant.name_of_plant
-        url_img = plant.pic_of_plant
+def download_main_img() -> None:
+    """
+    Uploading images by link from the database.
+    """
+    for item in models.HousePlants.objects.all():
+        name_img = item.name_of_plant
+        url_img = item.pic_of_plant
         full_path = '/home/elena/pythonProject/site/project/finder/static/main img/' + name_img + '.jpg'
         urllib.request.urlretrieve(url_img, full_path)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # create_short_name()
-        # download_main_img()
         # links = get_links_pagination()
         # get_links_to_all_items(links)
         # s = get_description_items()
         # create_plant_instances(s)
+        # create_short_name()
+        # download_main_img()
+
+
+
 
