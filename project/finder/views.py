ada7971a7fd0208e24f5fe061e9f9e3dad61a524
ElenaGlu <shishalovae@mail.ru>
2023-07-07 16:20:21 +0500

    changed the names and logic in views

--- project/finder/views.py
+++ project/finder/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import get_object_or_404, render
 
+
 from .models import HousePlants
 from .forms import CheckBoxForm
 from .finder_services import creates_default_filters_for_start_page, generates_page_by_filters, \
     changing_value_of_filters
 
 
-def select_plant(request):
-    form = CheckBoxForm(request.POST or None)
-    page_number = request.GET.get("page")
-    page_obj = generates_page_by_filters(page_number, creates_default_filters_for_start_page())
+def displays_plants_by_filters(request):
+    if request.method == "GET":
+        form = CheckBoxForm(request.POST or None)
+        page_number = request.GET.get("page")
+        page_obj = generates_page_by_filters(page_number, creates_default_filters_for_start_page())
 
     if request.method == "POST":
         form = CheckBoxForm(request.POST or None)
+        page_number = request.GET.get("page")
         if form.is_valid():
             page_obj = generates_page_by_filters(page_number, changing_value_of_filters(request))
-        else:
-            print(form.errors)
 
-    return render(request, 'finder/select_plant.html', {'form': form, 'page_obj': page_obj})
+    return render(request, 'finder/filtration_of_plants.html', {'form': form, 'page_obj': page_obj})
 
 
-def description_plant(request, pk):
+def displays_description_of_specific_plant(request, pk):
     plant = get_object_or_404(HousePlants, pk=pk)
     return render(request, 'finder/description_plant.html', {'plant': plant})
--- project/vff/urls.py
+++ project/vff/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from finder import views
 
 urlpatterns = [
-    path('', views.select_plant, name='select_plant'),
-    path('<int:pk>/', views.description_plant, name='description_plant'),
+    path('', views.displays_plants_by_filters, name='displays_plants_by_filters'),
+    path('<int:pk>/', views.displays_description_of_specific_plant, name='displays_description_of_specific_plant'),
     path('admin/', admin.site.urls),
 ]
