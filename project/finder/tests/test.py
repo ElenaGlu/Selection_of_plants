b732417b629bf5f2053391dd38804a9194484364
ElenaGlu <shishalovae@mail.ru>
2023-10-25 18:40:23 +0500

    OK

--- project/finder/tests/test.py
+++ project/finder/tests/test.py
@@ -1,9 +1,11 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
 
 
-class FinderTests(SimpleTestCase):
-
+class FiltersTests(TestCase):
+    """
+    Requests a html page and checks its status code and the correctness of the template.
+    """
     def test_filtration_of_plants(self):
         response = self.client.get(reverse("displays_plants_by_filters"))
         self.assertEqual(response.status_code, 200)
